576428e66f745c36c35365887be71ff34482da62
Febe Calderon <samcalma1@gmail.com>
2022-04-04 15:41:17 +0200

    js axios

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("JS OK!");
+
+//link introdotto dalla comunicazione esercizio
+axios
+  .get("https://flynn.boolean.careers/exercises/api/random/mail")
+  .then(function (response) {
+    const result = response.data;
+    console.log(result);
+  });
